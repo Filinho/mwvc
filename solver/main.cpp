7fbbedb3179b129dac325c309d9a837d70a33331
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-11-24 20:33:43 -0300

    feat: adjust main

--- solver/main.cpp
+++ solver/main.cpp
@@ -6,10 +6,22 @@ int main(int argc, char* argv[]) {
 	Graph g;
 
 	instance.parse(argv[1]);
-	State s;
-	Solver solver;
-	cout << "ratio solution: " << solver.ratioEdge(s) << endl;	
+	State s1;
 	State s2;
-	cout << "random solution: "<< solver.randomEdge(s2) <<endl;
-	cout<< s.verify() << s.verify();
+	Solver solver;
+	 int rn = solver.randomEdge(s1);
+	 int ratio = solver.ratioEdge(s2);
+	cout << argv[1] << endl;
+	 if (!s1.verify()) {
+	 	cout << "rnInvalid ";
+	 }
+	 else {
+	 	cout << rn << " " << s1.timeSpent << " ";
+	 }
+	 if (!s2.verify()) {
+	 	cout << "ratioInvalid" << endl;
+	 }
+	 else {
+	 	cout << ratio << " " << s2.timeSpent << endl;
+	 }
 }
