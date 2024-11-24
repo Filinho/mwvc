9fb5c89a7ad9e0a652f7b9ade27ba71d1c692ab0
Maicon Leles <119192182+maiconpml@users.noreply.github.com>
2024-11-24 20:33:25 -0300

    feat: add optimal solver exptime algorithm

--- solver/solver.hpp
+++ solver/solver.hpp
@@ -1,4 +1,9 @@
+#pragma once
+
 #include "State.hpp"
+#include "include/pcg_random.hpp"
+#include <random>
+#include <chrono>
 
 class Solver{
     public:
@@ -6,16 +11,25 @@ class Solver{
     Solver(){};
 
     int randomEdge(State & solution){
+
+      chrono::high_resolution_clock::time_point tpStart = chrono::high_resolution_clock::now();
+
+      // generate random seed if available
+      pcg_extras::seed_seq_from<std::random_device> seed_source;
+
+      // create random engine
+      pcg32 rng(seed_source);
+        
         // pick random edge
         int nEdges = instance.nEdges;
         int count = 0;
         vector<set<int>> auxAdjList = instance.adjList;
         while(nEdges > 0){
-            int v1 = rand() % instance.nVertex;
+            int v1 = rng(instance.nVertex);
             int v2;
 
             if(auxAdjList[v1].size()){   
-                v2 = rand() % auxAdjList[v1].size();
+                v2 = rng(auxAdjList[v1].size());
                 if (v2 >= 0 && v2 < auxAdjList[v1].size()) {
                     auto it = auxAdjList[v1].begin();
                     advance(it, v2);
@@ -42,24 +56,28 @@ class Solver{
                 solution.selected[v2] = 1;
             }
         }
-        
 
-        for(int i : solution.selected){
-            if(i) count += instance.weight[i];
-        } 
+        solution.timeSpent = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - tpStart).count();
+        
+        for (int i = 0; i < instance.nVertex; i++) {
+          if (solution.selected[i] == 1) count += instance.weight[i];
+        }
         return count;
     }
 
     int ratioEdge(State & solution){
+
+      chrono::high_resolution_clock::time_point tpStart = chrono::high_resolution_clock::now();
+
         vector<int> order;
         vector<set<int>> auxAdjList = instance.adjList;
         order.resize(instance.nVertex);
         order[0] = 0;
      
         for(int i = 1 ; i< instance.nVertex;i++){
-            
+            if (instance.adjList[i].size() == 0) continue;
             int j = i -1;
-            while( j >=0  && (float) instance.weight[i]/instance.adjList[i].size() < (float)  instance.weight[order[j]]/instance.adjList[order[j]].size()){
+            while( j >=0 && (float) instance.weight[i]/instance.adjList[i].size() < (float)  instance.weight[order[j]]/instance.adjList[order[j]].size()){
                 order[j+1] = order[j];
                 j--;
             }
@@ -80,12 +98,77 @@ class Solver{
             auxAdjList[v].clear();
             auxI++;
         }
+
+        solution.timeSpent = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - tpStart).count();
+
         int count = 0;
         for(int i = 0 ; i< instance.nVertex;i++){
             if(solution.selected[i] == 1) count += instance.weight[i];
         }
+
         return count;
     }
 
+    void makeCombiUtil(vector<vector<int> >& ans, vector<int>& tmp, int n, int left, int k)
+    {
+      // Pushing this vector to a vector of vector
+      if (k == 0) {
+        ans.push_back(tmp);
+        return;
+      }
+
+      // i iterates from left to n. First time
+      // left will be 1
+      for (int i = left; i <= n; ++i)
+      {
+        tmp.push_back(i);
+        makeCombiUtil(ans, tmp, n, i + 1, k - 1);
+
+        // Popping out last inserted element
+        // from the vector
+        tmp.pop_back();
+      }
+    }
+
+    // Prints all combinations of size k of numbers
+    // from 1 to n.
+    vector<vector<int> > makeCombi(int n, int k)
+    {
+      vector<vector<int> > ans;
+      vector<int> tmp;
+      makeCombiUtil(ans, tmp, n, 1, k);
+      return ans;
+    }
+
+
+    unsigned opt(State& solution) {
+
+      chrono::high_resolution_clock::time_point tpStart = chrono::high_resolution_clock::now();
+
+      unsigned cost = UINT32_MAX;
+
+      for (unsigned k = 1; k < instance.nVertex; ++k) {
+        vector<vector<int>> possibleSol = makeCombi(instance.nVertex, k);
+        for (unsigned i = 0; i < possibleSol.size(); ++i) {
+          State auxSol;
+          unsigned auxCost = 0;
+          for (unsigned j = 0; j < possibleSol[i].size(); ++j) {
+            //cout << possibleSol[i][j]-1 << " ";
+            auxCost += instance.weight[possibleSol[i][j]-1];
+            auxSol.selected[possibleSol[i][j]-1] = true;
+          }
+          //cout << endl;
+
+          if (auxCost < cost && auxSol.verify()) {
+            solution = auxSol;
+            cost = auxCost;
+          }
+        }
+      }
+
+      solution.timeSpent = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - tpStart).count();
+
+      return cost;
+    }
 };
 
